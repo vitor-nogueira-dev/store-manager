1451d06138fc4240e69631b82052b9c869467d14
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-28 22:21:49 -0300

    fix: remove comments function updateSale

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,42 +1,20 @@
-const Model = require('../models');
+const Model = require('../models/store.manager.models');
 
-const verifyProductId = (allProducts, arrayBody) =>
-  arrayBody.every((item) =>
-    allProducts.some((produto) => item.productId === produto.id));
+const Helpers = {
+  verifyProductId: (allProducts, arrayBody) =>
+    arrayBody.every((item) =>
+      allProducts.some((produto) => item.productId === produto.id)),
 
-const insertedSales = async (arrayBody, insertId) => {
-  const inserts = await Promise.all(
-    arrayBody.map(async (elem) => {
-      const { productId, quantity } = elem;
-      const insertSale = await Model.insertProductsSales(
-        insertId,
-        productId,
-        quantity,
-      );
-      console.log(insertSale, 'insertSale');
-    }),
-  );
-  console.log(inserts, 'inserts');
-  return {
-    id: insertId,
-    itemsSold: arrayBody,
-  };
-};
-const updatedSales = async (arrayBody, saleId) => {
-  const inserts = await arrayBody.map(async (elem) => {
-    const { productId, quantity } = elem;
-    const insertSale = await Model.updateSaleById(productId, quantity, saleId);
-    return insertSale;
-  });
-  await Promise.all(inserts);
-  return {
-    id: inserts,
-    itemsSold: arrayBody,
-  };
+  insertedSales: async (arrayBody, insertId) => {
+    await Promise.all(
+      arrayBody.map(({ productId, quantity }) =>
+        Model.insertProductsSales(insertId, productId, quantity)),
+    );
+    return {
+      id: insertId,
+      itemsSold: arrayBody,
+    };
+  },
 };
 
-module.exports = {
-  verifyProductId,
-  insertedSales,
-  updatedSales,
-};
+module.exports = Helpers;
